3acda28e60dfd6051cdcd35b28f819b0f9b72877
FLORIN-GABRIEL CABAUA <79976336+CabauaFlorin@users.noreply.github.com>
2021-11-14 19:54:11 +0200

    Plans

---
+++ ChronosAPI/Controllers/PlansController.cs
@@ -0,0 +1,133 @@
+﻿using ChronosAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChronosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlansController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public PlansController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+
+        public JsonResult GetPlans()
+        {
+            string query = @"SELECT * from dbo.Plans";
+            DataTable table = new DataTable();
+            string sqlSource = _configuration.GetConnectionString("ChronosDBCon");
+            SqlDataReader reader;
+            using (SqlConnection my_connection = new SqlConnection(sqlSource))
+            {
+                my_connection.Open();
+                using (SqlCommand my_command = new SqlCommand(query, my_connection))
+                {
+                    reader = my_command.ExecuteReader();
+                    table.Load(reader);
+                    reader.Close();
+                    my_connection.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        public JsonResult PostPlan(Plans plan)
+        {
+            string query = @"INSERT INTO dbo.Plans
+                            ([Title],
+                             [CreatedAt],
+                             [Description])
+                            VALUES
+                             (@Title,
+                              @CreatedAt,
+                              @Description)";
+
+            DataTable table = new DataTable();
+            string sqlSource = _configuration.GetConnectionString("ChronosDBCon");
+            SqlDataReader reader;
+            using (SqlConnection my_connection = new SqlConnection(sqlSource))
+            {
+                my_connection.Open();
+                using (SqlCommand my_command = new SqlCommand(query, my_connection))
+                {
+                    my_command.Parameters.AddWithValue("@Title", plan.Title);
+                    my_command.Parameters.AddWithValue("@CreatedAt", plan.CreatedAt);
+                    my_command.Parameters.AddWithValue("@Description", plan.Description);
+
+                    reader = my_command.ExecuteReader();
+                    table.Load(reader);
+                    reader.Close();
+                    my_connection.Close();
+                }
+            }
+            return new JsonResult("Insert succesfull!");
+        }
+
+        [HttpPut]
+
+        public JsonResult UpdatePlan(Plans plan)
+        {
+            string query = @"UPDATE dbo.Plan
+                            set
+                            Title=@Title,
+                            CreatedAt=@CreatedAt,
+                            Description=@Description
+                            where PlanID=@PlanID";
+
+            DataTable table = new DataTable();
+            string sqlSource = _configuration.GetConnectionString("ChronosDBCon");
+            SqlDataReader reader;
+            using (SqlConnection my_connection = new SqlConnection(sqlSource))
+            {
+                my_connection.Open();
+                using (SqlCommand my_command = new SqlCommand(query, my_connection))
+                {
+                    my_command.Parameters.AddWithValue("@PlanID", plan.PlanId);
+                    reader = my_command.ExecuteReader();
+                    table.Load(reader);
+                    reader.Close();
+                    my_connection.Close();
+                }
+            }
+            return new JsonResult("Update succesfull!");
+        }
+
+        [HttpDelete]
+
+        public JsonResult DeletePlan(Plans plan)
+        {
+            string query = @"DELETE FROM dbo.Plans
+                            where PlanID=@PlanID";
+
+            DataTable table = new DataTable();
+            string sqlSource = _configuration.GetConnectionString("ChronosDBCon");
+            SqlDataReader reader;
+            using (SqlConnection my_connection = new SqlConnection(sqlSource))
+            {
+                my_connection.Open();
+                using (SqlCommand my_command = new SqlCommand(query, my_connection))
+                {
+                    my_command.Parameters.AddWithValue("@PlanID", plan.PlanId);
+                    reader = my_command.ExecuteReader();
+                    table.Load(reader);
+                    reader.Close();
+                    my_connection.Close();
+                }
+            }
+            return new JsonResult("Delete succesfull!");
+        }
+    }
+}
---
+++ ChronosAPI/Models/Plans.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChronosAPI.Models
+{
+    public class Plans
+    {
+        public int PlanId { get; set; }
+
+        public String Title { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public String Description { get; set; }
+    }
+}

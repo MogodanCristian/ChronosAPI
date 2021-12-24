e088070572ad6bfc9ab8f184595c124f7df41423
androcpp <georgeandronache.cpp@gmail.com>
2021-12-24 20:24:08 +0200

    Email Route Added

---
+++ ChronosAPI/Controllers/EmailController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+
+namespace ChronosAPI.Controllers
+{
+    [Route("api/email")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult sendEmailViaWebApi()
+        {
+            try
+            {
+                string subject = "Email Subject";
+                string body = "Email body";
+                string FromMail = "steelparrot.inc@gmail.com";
+                string emailTo = "georgeandronache.cpp@gmail.com";
+                MailMessage mail = new MailMessage();
+                SmtpClient client = new SmtpClient("smtp.gmail.com");
+                mail.From = new MailAddress(FromMail);
+                mail.To.Add(emailTo);
+                mail.Subject = subject;
+                mail.Body = body;
+                client.Port = 587;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new System.Net.NetworkCredential("steelparrot.inc@gmail.com", "papagaluu1$");
+                client.EnableSsl = true;
+                client.Send(mail);
+                return Ok("Mail Sent");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+           
+        }
+
+        private string createEmailBody(string userName, string message)
+        {
+            string body = message;
+            return body;
+        }
+    }
+}
---
+++ ChronosAPI/Models/EmailModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChronosAPI.Models
+{
+    public class EmailModel
+    {
+        [Required]
+        public string toname { get; set; }
+        [Required, EmailAddress]
+        public string toemail { get; set; }
+        [Required]
+        public string subject { get; set; }
+        [Required]
+        public string message { get; set; }
+    }
+}
